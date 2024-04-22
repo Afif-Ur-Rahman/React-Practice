349b24e2838595b86d08ec88121b10b2e67d5c22
Afif_Ur_Rahman <afifurrahman444@gmail.com>
2024-04-22 16:36:07 +0500

    Renamed to Counter, Added Vite App

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
   const [isError, setIsError] = useState(false);
+  const [isErrors, setIsErrors] = useState(false)
   return (
     <>
       <h2>Counter</h2>
@@ -13,17 +14,26 @@ function App() {
           Number Can&apos;t be less than 0.
         </div>
       )}
+      {isErrors && (
+        <div style={{ color: "red", marginTop: "-7px", fontSize: "14px" }}>
+          Number Can&apos;t be greater than 5.
+        </div>
+      )}
       <div>
         <button
           onClick={() => {
             setIsError(false);
-            setCount(count + 1);
+            if (count === 5) {
+              setIsErrors(true);
+            } else {
+            setCount(count + 1);}
           }}
         >
           Add
         </button>
         <button
           onClick={() => {
+            setIsErrors(false);
             if (count === 0) {
               setIsError(true);
             } else {
