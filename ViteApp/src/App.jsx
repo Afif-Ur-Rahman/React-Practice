87af3e7a42dc92da7e12773b00cbe638a4ed414d
Afif_Ur_Rahman <afifurrahman444@gmail.com>
2024-04-23 16:52:41 +0500

    fixed some issues in Color Generator

--- ViteApp/src/App.jsx
+++ ViteApp/src/App.jsx
@@ -8,36 +8,40 @@ const App = () => {
   const handleColorChange = () => {
     const color = "#" + Math.floor(Math.random() * 16777215).toString(16);
     setColor(color);
-    document.body.style.backgroundColor = color;
   };
   const handleCopy = () => {
     navigator.clipboard.writeText(color);
     setIsCopied(true);
     setTimeout(() => {
       setIsCopied(false);
-    }, 1000)
-  }
+    }, 1000);
+  };
 
   return (
     <>
-      <div className="flex justify-center items-center flex-col font-mono bg-white p-2 rounded-lg w-1/4 my-0 mx-auto bg-opacity-70">
-        <h1 className="font-mono text-xl font-bold">Color Changer</h1>
-        <div className="flex justify-center items-center">
-          <button
-            className="p-3 rounded-3xl text-black m-1 bg-transparent"
-            onClick={handleColorChange}
-          >
-            {color}
-          </button>
-          <button
-            className="p-3 rounded-3xl w-fit m-1 flex justify-center items-center bg-transparent tooltip"
-            onClick={handleCopy}
-          >
-            <img src={Clipboard} alt="Clipboard Logo" />
-            <span className={`tooltiptext ${isCopied ? "block" : "hidden"}`}>
-              Copied
-            </span>
-          </button>
+      <div
+        className="flex justify-center flex-wrap items-center flex-col font-mono w-full h-screen"
+        style={{ backgroundColor: color }}
+      >
+        <div className="bg-white rounded-lg bg-opacity-70 w-max p-4">
+          <h1 className="font-mono text-xl font-bold pb-2">Color Generator</h1>
+          <div className="flex justify-center items-center">
+            <button
+              className="rounded-3xl text-black bg-transparent"
+              onClick={() => handleColorChange()}
+            >
+              {color}
+            </button>
+            <button
+              className="rounded-3xl flex justify-center items-center bg-transparent tooltip"
+              onClick={() => handleCopy()}
+            >
+              <img src={Clipboard} alt="Clipboard Logo" />
+              <span className={`tooltiptext ${isCopied ? "block" : "hidden"}`}>
+                Copied!
+              </span>
+            </button>
+          </div>
         </div>
       </div>
     </>
