36e29018fcdfa4c25fe837bf663862e49a04492f
Afif_Ur_Rahman <afifurrahman444@gmail.com>
2024-05-02 19:34:31 +0500

    Modified Not Found

--- React Router/src/components/404/404NotFound.jsx
+++ React Router/src/components/404/404NotFound.jsx
@@ -1,16 +1,20 @@
-import { Link, useNavigate } from 'react-router-dom';
-import { FaArrowLeft } from 'react-icons/fa';
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
 
 function NotFound() {
-    const navigate = useNavigate();
-    const GoBack = () => {
-        navigate(-1);
-    }
+  const navigate = useNavigate();
+  const location = useLocation();
+  const GoBack = () => {
+    navigate(-1);
+  };
 
   return (
     <div className="my-12 flex items-center justify-center px-2 md:my-24 md:px-0">
-      <div className="lg:flex lg:items-center lg:space-x-10" style={{height: "50vh"}}>
-      <img
+      <div
+        className="lg:flex lg:items-center lg:space-x-10"
+        style={{ height: "50vh" }}
+      >
+        <img
           className="sm:w-96 w-48"
           src="https://i.ibb.co/2M7rtLk/Remote1.png"
           alt="image2"
@@ -18,11 +22,10 @@ function NotFound() {
         <div>
           <p className="mt-6 text-sm font-semibold text-black">404 error</p>
           <h1 className="mt-3 text-2xl font-semibold text-gray-800 md:text-3xl">
-            We can&apos;t find that page
+            We can&apos;t find {location.pathname}
           </h1>
           <p className="mt-4 text-gray-500">
-            Sorry, the page you are looking for doesn&apos;t exist or has been
-            moved.
+            Sorry, the page {location.pathname} does not exist or has been moved.
           </p>
           <div className="flex items-center lg:order-2 mt-4">
             <Link
@@ -33,7 +36,7 @@ function NotFound() {
               Go back
             </Link>
             <Link
-            to="/contact"
+              to="/contact"
               className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
             >
               Contact us
--- React Router/src/components/Header/Header.jsx
+++ React Router/src/components/Header/Header.jsx
@@ -1,6 +1,19 @@
+import { useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 
-function Header() {
+const Header = () => {
+  const [followers, setFollowers] = useState(0);
+  const facebookFollowers = async () => {
+    const response = await fetch("https://api.github.com/users/afif-Ur-Rahman");
+    const data = await response.json();
+    console.log(data);
+    setFollowers(data.followers || 0);
+  };
+
+  useEffect(() => {
+    facebookFollowers();
+  }, []);
+
   return (
     <header className="shadow sticky z-50 top-0">
       <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
@@ -12,6 +25,7 @@ function Header() {
               alt="Logo"
             />
           </Link>
+          <div>Github Followers: {followers}</div>
           <div className="flex items-center lg:order-2">
             <Link
               to="#"
@@ -27,15 +41,17 @@ function Header() {
             </Link>
           </div>
           <div
-            className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+            className="border-solid border-2 bg-orange-700 border-orange-700 rounded-3xl hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
             id="mobile-menu-2"
           >
-            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:mt-0">
               <li>
                 <NavLink
-                to="/"
-                  className={({isActive}) =>
-                    `block py-2 pr-4 pl-3 duration-200 ${isActive? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                  to="/"
+                  className={({ isActive }) =>
+                    `p-2 block duration-200 ${
+                      isActive ? "text-orange-700 bg-white" : "text-white"
+                    } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-white lg:border-0 hover:text-orange-700 rounded-3xl`
                   }
                 >
                   Home
@@ -43,9 +59,11 @@ function Header() {
               </li>
               <li>
                 <NavLink
-                to="/about"
-                  className={({isActive}) =>
-                    `block py-2 pr-4 pl-3 duration-200 ${isActive? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                  to="/about"
+                  className={({ isActive }) =>
+                    `p-2 block duration-200 ${
+                      isActive ? "text-orange-700 bg-white" : "text-white"
+                    } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-white lg:border-0 hover:text-orange-700 rounded-3xl`
                   }
                 >
                   About Us
@@ -53,9 +71,11 @@ function Header() {
               </li>
               <li>
                 <NavLink
-                to="/contact"
-                  className={({isActive}) =>
-                    `block py-2 pr-4 pl-3 duration-200 ${isActive? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                  to="/contact"
+                  className={({ isActive }) =>
+                    `p-2 block duration-200 ${
+                      isActive ? "text-orange-700 bg-white" : "text-white"
+                    } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-white lg:border-0 hover:text-orange-700 rounded-3xl`
                   }
                 >
                   Contact Us
@@ -67,6 +87,6 @@ function Header() {
       </nav>
     </header>
   );
-}
+};
 
 export default Header;
